32540ce1148f73b2776d4d264d035759490aa7c8
Raphael-Mendess <raphael.hkiller@gmail.com>
2026-05-06 22:50:03 -0300

    Project created

---
+++ src/main/java/com/raphaeldev/systemproduct/SystemproductApplication.java
@@ -0,0 +1,13 @@
+package com.raphaeldev.systemproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemproductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemproductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raphaeldev/systemproduct/SystemproductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raphaeldev.systemproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemproductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
